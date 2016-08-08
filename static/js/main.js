da8bfc9054fc97ddf81da9ecda68fecff5ad8b9c
mobeets <mobeets@gmail.com>
2016-08-08 15:33:08 -0400

    calculating valid ranges of values given current vals, fixes #9

--- static/js/main.js
+++ static/js/main.js
@@ -1,140 +1,3 @@
-// var allRecipes = [
-//     {
-//         "url": "http://food52.com/blog/2262_tender_yellow_cake", 
-//         "name": "Tender Yellow Cake", 
-//         "ingredients": [
-//             {'name': 'eggs', 'qty': 3, 'unit': ''},
-//             {'name': 'sugar', 'qty': 1.5, 'unit': 'cup'},
-//             {'name': 'flour', 'qty': 2.25, 'unit': 'cup'},
-//             {'name': 'baking powder', 'qty': 3, 'unit': 'tsp'},
-//             {'name': 'salt', 'qty': 1, 'unit': 'tsp'},
-//             {'name': 'milk', 'qty': 1, 'unit': 'cup'},
-//             {'name': 'vanilla', 'qty': 1.5, 'unit': 'tsp'}
-//         ]
-//     }, 
-//     {
-//         "url": "http://www.funteapartyideas.com/basic-yellow-cake-recipe.html", 
-//         "name": "Basic Yellow Cake", 
-//         "ingredients": [
-//             {'name': 'eggs', 'qty': 5, 'unit': ''},
-//             {'name': 'sugar', 'qty': 2.5, 'unit': 'cup'},
-//             {'name': 'flour', 'qty': 3, 'unit': 'cup'},
-//             {'name': 'baking powder', 'qty': 1, 'unit': 'tsp'},
-//             {'name': 'salt', 'qty': 0.25, 'unit': 'tsp'},
-//             {'name': 'butter', 'qty': 1.5, 'unit': 'cup'},
-//             {'name': 'milk', 'qty': 1, 'unit': 'cup'},
-//             {'name': 'vanilla', 'qty': 1, 'unit': 'tsp'}
-//         ]
-//     },
-//     {
-//         "url": "http://thecakeblog.com/2014/05/classic-yellow-cake-with-fudge-frosting.html", 
-//         "name": "Classic Yellow Cake with Fudge Frosting", 
-//         "ingredients": [
-//             {'name': 'eggs', 'qty': 5, 'unit': ''},
-//             {'name': 'sugar', 'qty': 1.75, 'unit': 'cup'},
-//             {'name': 'flour', 'qty': 3, 'unit': 'cup'},
-//             {'name': 'baking powder', 'qty': 1, 'unit': 'Tbsp'},
-//             {'name': 'salt', 'qty': 0.5, 'unit': 'tsp'},
-//             {'name': 'butter', 'qty': 1, 'unit': 'cup'},
-//             {'name': 'vanilla', 'qty': 1, 'unit': 'tsp'}
-//         ]
-//     }, 
-//     {
-//         "url": "http://www.thevintagemixer.com/2014/01/yellow-cake-with-chocolate-frosting-recipe/", 
-//         "name": "Perfect Yellow Cake", 
-//         "ingredients": [
-//             {'name': 'eggs', 'qty': 4, 'unit': ''},
-//             {'name': 'sugar', 'qty': 2, 'unit': 'cup'},
-//             {'name': 'flour', 'qty': 2.67, 'unit': 'cup'},
-//             {'name': 'baking powder', 'qty': 2, 'unit': 'tsp'},
-//             {'name': 'salt', 'qty': 0.5, 'unit': 'tsp'},
-//             {'name': 'butter', 'qty': 1, 'unit': 'cup'},
-//             {'name': 'milk', 'qty': 1, 'unit': 'cup'},
-//             {'name': 'vanilla', 'qty': 1.5, 'unit': 'tsp'}
-//         ]
-//     }, 
-//     {
-//         "url": "http://lovefoodies.com/home-made-yellow-cake-mix.html", 
-//         "name": "Home Made Yellow Cake Mix", 
-//         "ingredients": [
-//             {'name': 'eggs', 'qty': 3, 'unit': ''},
-//             {'name': 'sugar', 'qty': 2.25, 'unit': 'cup'},
-//             {'name': 'flour', 'qty': 2.5, 'unit': 'cup'},
-//             {'name': 'baking powder', 'qty': 2, 'unit': 'tsp'},
-//             {'name': 'salt', 'qty': 1, 'unit': 'tsp'},
-//             {'name': 'butter', 'qty': 0.33, 'unit': 'cup'},
-//             {'name': 'milk', 'qty': 1, 'unit': 'cup'},
-//         ]
-//     }, 
-//     {
-//         "url": "http://onetoughcookienyc.com/2010/03/cake-is-the-new-denim/", 
-//         "name": "Yellow Butter Cake", 
-//         "ingredients": [
-//             {'name': 'eggs', 'qty': 6, 'unit': ''},
-//             {'name': 'sugar', 'qty': 1.5, 'unit': 'cup'},
-//             {'name': 'flour', 'qty': 3, 'unit': 'cup'},
-//             {'name': 'baking powder', 'qty': 1.33, 'unit': 'Tbsp'},
-//             {'name': 'salt', 'qty': 0.75, 'unit': 'tsp'},
-//             {'name': 'butter', 'qty': 0.75, 'unit': 'cup'},
-//             {'name': 'milk', 'qty': 1, 'unit': 'cup'},
-//             {'name': 'vanilla', 'qty': 2.25, 'unit': 'tsp'}
-//         ]
-//     },
-//     {
-//         "url": "http://allrecipes.com.au/recipe/20598/plain-yellow-cake.aspx", 
-//         "name": "Plain Yellow Cake", 
-//         "ingredients": [
-//             {'name': 'eggs', 'qty': 2, 'unit': ''},
-//             {'name': 'sugar', 'qty': 1.67, 'unit': 'cup'},
-//             {'name': 'flour', 'qty': 3, 'unit': 'cup'},
-//             {'name': 'baking powder', 'qty': 2.25, 'unit': 'tsp'},
-//             {'name': 'salt', 'qty': 0.5, 'unit': 'tsp'},
-//             {'name': 'butter', 'qty': 1, 'unit': 'cup'},
-//             {'name': 'milk', 'qty': 1.33, 'unit': 'cup'},
-//             {'name': 'vanilla', 'qty': 2, 'unit': 'tsp'}
-//         ]
-//     }, 
-//     {
-//         "url": "http://www.thekitchn.com/recipe-diy-yell-1-14637", 
-//         "name": "Quick Yellow Cake", 
-//         "ingredients": [
-//             {'name': 'eggs', 'qty': 3, 'unit': ''},
-//             {'name': 'sugar', 'qty': 1.5, 'unit': 'cup'},
-//             {'name': 'flour', 'qty': 2.25, 'unit': 'cup'},
-//             {'name': 'baking powder', 'qty': 3.5, 'unit': 'tsp'},
-//             {'name': 'salt', 'qty': 1, 'unit': 'tsp'},
-//             {'name': 'butter', 'qty': 0.5, 'unit': 'cup'},
-//             {'name': 'milk', 'qty': 1.25, 'unit': 'cup'},
-//             {'name': 'vanilla', 'qty': 1, 'unit': 'tsp'}
-//         ]
-//     }
-// ];
-
-// var recipe1 = [
-//   {'name': 'flour', 'qty': 2, 'unit': 'cup'},
-//   {'name': 'sugar', 'qty': 1, 'unit': 'cup'},
-//   {'name': 'butter', 'qty': 0.5, 'unit': 'cup'},
-// ];
-// var recipe2 = [
-//   {'name': 'flour', 'qty': 3, 'unit': 'cup'},
-//   {'name': 'sugar', 'qty': 1, 'unit': 'cup'},
-//   {'name': 'butter', 'qty': 1, 'unit': 'cup'},
-// ];
-// var recipe3 = [
-//   {'name': 'flour', 'qty': 2, 'unit': 'cup'},
-//   {'name': 'sugar', 'qty': 0.5, 'unit': 'cup'},
-//   {'name': 'butter', 'qty': 0.25, 'unit': 'cup'},
-// ];
-// var recipe4 = [
-//   {'name': 'flour', 'qty': 2, 'unit': 'cup'},
-//   {'name': 'sugar', 'qty': 0.5, 'unit': 'cup'},
-//   {'name': 'butter', 'qty': 0.5, 'unit': 'cup'},
-// ];
-
-// var recipes = addAllRawQtys([recipe1, recipe2, recipe3, recipe4]);
-// var recipes = addAllRawQtys(recipes);
-// var ranges = allRatioRanges(recipes);
-// var recipe = recipes[0];
 
 var recipes = [];
 for (var i=0; i<allRecipes.length; i++) {
@@ -144,44 +7,6 @@ for (var i=0; i<allRecipes.length; i++) {
 var recipes = addAllRawQtys(recipes);
 var ranges = allRatioRanges(recipes);
 var recipe = recipes[0];
-
-function oobsToColors(oobs) {
-  tooLow = [];
-  tooHigh = [];
-  for (var i=0; i<oobs.length; i++) {
-    oob = oobs[i];
-    nm1 = oob.ratio.a;
-    nm2 = oob.ratio.b;
-    if (oob.ratio.r < oob.minVal) {
-      nm3 = nm1; nm1 = nm2; nm2 = nm3;
-    }
-    tooHigh.push(nm1);
-    tooLow.push(nm2);
-  }
-
-  for (var i=0; i<recipe.length; i++) {
-    key = 'background';
-    val = 'none';
-    nm = recipe[i].name;
-    if ($.inArray(nm, tooLow) > -1 && $.inArray(nm, tooHigh) > -1) {
-      val = 'ban-circle';
-    } else if ($.inArray(nm, tooLow) > -1) {
-      val = 'arrow-up';
-    } else if ($.inArray(nm, tooHigh) > -1) {
-      val = 'arrow-down';
-    } else {
-      val = '';
-    }
-    if (val.length > 0) {
-      glyph = '<span class="glyphicon glyphicon-' + val + '" aria-hidden="true"></span>';
-    } else {
-      glyph = '';
-    }
-    $('#item-' + nm + ' .glyphs').html(glyph);
-    $('#item-' + nm).css(key, val);
-  }
-  
-}
 
 function initSlider(selector, valSelector, initVal, itemName, itemIndex) {
   $(selector).slider({
@@ -193,10 +18,11 @@ function initSlider(selector, valSelector, initVal, itemName, itemIndex) {
       $(valSelector).text(ui.value);
       // update value in recipe
       recipe[itemIndex].qtyRaw = ui.value;
-      oobs = outOfBoundsRatiosInRecipe(recipe, ranges);
-      msgs = oobsToMsgs(oobs, recipes.length);
+      checkOutOfBoundsIngrients(recipe, ranges);
+      // oobs = outOfBoundsRatiosInRecipe(recipe, ranges);
+      // msgs = oobsToMsgs(oobs, recipes.length);
       // $('.errors').html(msgs.join('<br>'));
-      oobsToColors(oobs);
+      // oobsToColors(oobs);
     }
   });
   val = $(selector).slider("value");
@@ -241,12 +67,10 @@ function initPresets(recipes) {
 function init() {
   // todo: handle ingredients in recipes
   
+  console.log(ranges);
   initPresets(recipes);
   initRecipe(recipe);
   $('.recipe-preset').click(selectPreset);
 }
-// var recipes = addAllRawQtys(recipes);
-// var ranges = allRatioRanges(recipes);
-// var recipe = recipes[0];
 
 $(document).ready(init);
--- static/js/ratios.js
+++ static/js/ratios.js
@@ -93,20 +93,147 @@ function oobsToMsgs(oobs, nRs) {
     return msgs;
 }
 
-function outOfBoundsRatiosInRecipe(recipe, ranges) {
-    ratios = getRatiosInRecipe(recipe);
-    oobs = [];
-    for (var i=0; i<ratios.length; i++) {
-        mn = ranges.min[ratios[i].name];
-        mx = ranges.max[ratios[i].name];
-        actual = ratios[i].r;
-        if (actual < mn || actual > mx) {
-            oobs.push({
-                'ratio': ratios[i],
-                'minVal': mn,
-                'maxVal': mx
-            });
-        }
+function getIngredsInRecipe(recipe) {
+    ingreds = [];
+    for (var i=0; i<recipe.length; i++) {
+        ingreds.push(recipe[i].name);
     }
-    return oobs;
+    return ingreds;
 }
+
+function getAllowedRangesInRecipe(recipe, ranges) {
+    ratios = getRatiosInRecipe(recipe);
+    ingreds = getIngredsInRecipe(recipe);
+    allMins = [];
+    allMaxs = [];
+    for (var i=0; i<ratios.length; i++) {
+        aNm = ratios[i].a;
+        bNm = ratios[i].b;
+
+        mnRatio = ranges.min[ratios[i].name];
+        mxRatio = ranges.max[ratios[i].name];
+        aVal = recipe[$.inArray(aNm, ingreds)].qtyRaw;
+        bVal = recipe[$.inArray(bNm, ingreds)].qtyRaw;
+
+        if ($.inArray(aNm, allMins) === -1) {
+            allMins[aNm] = [];
+            allMaxs[aNm] = [];
+        }
+        if ($.inArray(bNm, allMins) === -1) {
+            allMins[bNm] = [];
+            allMaxs[bNm] = [];
+        }
+
+        aMin = mnRatio*bVal;
+        aMax = mxRatio*bVal;
+        bMax = aVal/mnRatio;
+        bMin = aVal/mxRatio;
+
+        allMins[aNm].push(aMin);
+        allMaxs[aNm].push(aMax);
+        allMins[bNm].push(bMin);
+        allMaxs[bNm].push(bMax);
+    }
+
+    validRanges = [];
+    for (var i=0; i<ingreds.length; i++) {
+        mns = allMins[ingreds[i]];
+        mxs = allMaxs[ingreds[i]];
+        mn = Math.max(mns);
+        mx = Math.min(mxs);
+        // console.log([ingreds[i], mn, mx]);
+        // console.log([mns]);
+        // console.log([mxs]);
+        // console.log('-------');
+        validRanges[ingreds[i]] = [mn, mx];
+    }
+    console.log(validRanges);
+    return validRanges;
+}
+
+function markOutOfBoundsIngredients(recipe, validRanges) {
+    ingreds = getIngredsInRecipe(recipe);
+    for (var i=0; i<ingreds.length; i++) {
+        nm = ingreds[i];
+        val = recipe[$.inArray(nm, ingreds)].qtyRaw;
+        mn = validRanges[nm][0];
+        mx = validRanges[nm][1];
+        if (mn > mx) {
+            glyphId = 'ban-circle';
+        } else if (val < mn) {
+            glyphId = 'arrow-up';
+        } else if (val > mx) {
+            glyphId = 'arrow-down';
+        } else {
+            glyphId = '';
+        }
+        // console.log([nm, val, mn, mx, glyphId]);
+        if (glyphId.length > 0) {
+            glyph = '<span class="glyphicon glyphicon-' + glyphId + '" aria-hidden="true"></span>';
+        } else { glyph = ''; }
+        $('#item-' + nm + ' .glyphs').html(glyph);
+    }
+}
+
+function checkOutOfBoundsIngrients(recipe, ranges) {
+    validRanges = getAllowedRangesInRecipe(recipe, ranges);
+    markOutOfBoundsIngredients(recipe, validRanges);
+}
+
+// function outOfBoundsRatiosInRecipe(recipe, ranges) {
+
+//     ratios = getRatiosInRecipe(recipe);
+//     oobs = [];
+//     for (var i=0; i<ratios.length; i++) {
+//         mn = ranges.min[ratios[i].name];
+//         mx = ranges.max[ratios[i].name];
+//         actual = ratios[i].r;
+//         if (actual < mn || actual > mx) {
+//             oobs.push({
+//                 'ratio': ratios[i],
+//                 'minVal': mn,
+//                 'maxVal': mx
+//             });
+//         }
+//     }
+//     return oobs;
+// }
+
+
+// function oobsToColors(oobs) {
+//   tooLow = [];
+//   tooHigh = [];
+//   for (var i=0; i<oobs.length; i++) {
+//     oob = oobs[i];
+//     nm1 = oob.ratio.a;
+//     nm2 = oob.ratio.b;
+//     if (oob.ratio.r < oob.minVal) {
+//       nm3 = nm1; nm1 = nm2; nm2 = nm3;
+//     }
+//     tooHigh.push(nm1);
+//     tooLow.push(nm2);
+//   }
+
+//   for (var i=0; i<recipe.length; i++) {
+//     key = 'background';
+//     val = 'none';
+//     nm = recipe[i].name;
+//     if ($.inArray(nm, tooLow) > -1 && $.inArray(nm, tooHigh) > -1) {
+//       val = 'ban-circle';
+//     } else if ($.inArray(nm, tooLow) > -1) {
+//       val = 'arrow-up';
+//     } else if ($.inArray(nm, tooHigh) > -1) {
+//       val = 'arrow-down';
+//     } else {
+//       val = '';
+//     }
+//     if (val.length > 0) {
+//       glyph = '<span class="glyphicon glyphicon-' + val + '" aria-hidden="true"></span>';
+//     } else {
+//       glyph = '';
+//     }
+//     $('#item-' + nm + ' .glyphs').html(glyph);
+//     $('#item-' + nm).css(key, val);
+//   }
+  
+// }
